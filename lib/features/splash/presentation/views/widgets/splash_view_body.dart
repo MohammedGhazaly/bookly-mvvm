8a58d3d1958aec6579e04cb416a0f381947812c4
MohammedGhazaly <warendragon67@gmail.com>
2023-10-11 18:46:05 +0200

    Refactored navigation in our app

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,6 +1,8 @@
 import 'package:bookly_mvvm/core/utils/my_assets.dart';
+import 'package:bookly_mvvm/core/utils/my_routes.dart';
 import 'package:bookly_mvvm/features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -17,15 +19,11 @@ class _SplashViewBodyState extends State<SplashViewBody>
   @override
   void initState() {
     super.initState();
-    animationController =
-        AnimationController(vsync: this, duration: const Duration(seconds: 1));
-    slidingAnimation =
-        Tween<Offset>(begin: const Offset(0, 7), end: const Offset(0, 0))
-            .animate(animationController);
-    animationController.forward();
-    // slidingAnimation.addListener(() {
-    //   setState(() {});
-    // });
+    initSlidingAnimation();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      GoRouter.of(context).push(MyRouters.kHomveViewRoute);
+    });
   }
 
   @override
@@ -41,12 +39,21 @@ class _SplashViewBodyState extends State<SplashViewBody>
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Image.asset(MyAssets.logo),
+        Image.asset(MyAssets.kLogo),
         const SizedBox(
           height: 5,
         ),
         SlidingText(slidingAnimation: slidingAnimation)
       ],
     );
+  }
+
+  void initSlidingAnimation() {
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 7), end: const Offset(0, 0))
+            .animate(animationController);
+    animationController.forward();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
+import 'package:bookly_mvvm/core/utils/my_routes.dart';
 import 'package:bookly_mvvm/core/utils/my_theme.dart';
-import 'package:bookly_mvvm/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:get/get.dart';
 
 void main() {
   runApp(const BooklyApp());
@@ -17,10 +16,10 @@ class BooklyApp extends StatelessWidget {
     return ScreenUtilInit(
         designSize: const Size(360, 690),
         builder: (_, context) {
-          return GetMaterialApp(
+          return MaterialApp.router(
             debugShowCheckedModeBanner: false,
             theme: MyTheme.mainTheme,
-            home: const SplashView(),
+            routerConfig: MyRouters.routes,
           );
         });
   }
