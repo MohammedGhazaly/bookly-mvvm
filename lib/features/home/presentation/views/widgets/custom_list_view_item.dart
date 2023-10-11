2094f6e68d1f674fb07359d2dcab6b01a10cb3ec
MohammedGhazaly <warendragon67@gmail.com>
2023-10-11 22:47:20 +0200

    Created custom list view item

---
+++ lib/features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -0,0 +1,27 @@
+import 'package:bookly_mvvm/core/utils/my_assets.dart';
+import 'package:flutter/material.dart';
+
+class CustomListViewItem extends StatelessWidget {
+  const CustomListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.3,
+      child: AspectRatio(
+        aspectRatio: 2.6 / 4,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            image: const DecorationImage(
+              fit: BoxFit.fill,
+              image: AssetImage(
+                MyAssets.kTestImage,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
