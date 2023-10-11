78b9cd805879fac7303a14aac7a37f0c02b94858
MohammedGhazaly <warendragon67@gmail.com>
2023-10-11 23:03:43 +0200

    Added featured books list view to home view

--- lib/features/home/presentation/views/widgets/custom_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -1,13 +1,13 @@
 import 'package:bookly_mvvm/core/utils/my_assets.dart';
 import 'package:flutter/material.dart';
 
-class CustomListViewItem extends StatelessWidget {
-  const CustomListViewItem({super.key});
+class FeaturedBookItem extends StatelessWidget {
+  const FeaturedBookItem({super.key});
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.3,
+      height: MediaQuery.of(context).size.height * 0.25,
       child: AspectRatio(
         aspectRatio: 2.6 / 4,
         child: Container(
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -12,10 +12,36 @@ class HomeViewBody extends StatelessWidget {
     return SafeArea(
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 20.h),
-        child: const Column(
-          children: [CustomAppBar(), CustomListViewItem()],
+        child: Column(
+          children: [
+            const CustomAppBar(),
+            SizedBox(
+              height: 20.h,
+            ),
+            const FeaturedBooksListView(),
+          ],
         ),
       ),
+    );
+  }
+}
+
+class FeaturedBooksListView extends StatelessWidget {
+  const FeaturedBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.25,
+      child: ListView.builder(
+          itemCount: 10,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: FeaturedBookItem(),
+            );
+          }),
     );
   }
 }
