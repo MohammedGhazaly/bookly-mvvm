88efa6c514c2478f03a2ed1fbea08bcedf018988
MohammedGhazaly <warendragon67@gmail.com>
2023-10-11 12:24:23 +0200

    Added Utils for colors, theme and assets and finished a big chunk of splash view.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:bookly_mvvm/core/utils/my_theme.dart';
 import 'package:bookly_mvvm/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 void main() {
@@ -12,11 +14,14 @@ class BooklyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      theme: ThemeData(
-        useMaterial3: true,
-      ),
-      home: const SplashView(),
-    );
+    return ScreenUtilInit(
+        designSize: const Size(360, 690),
+        builder: (_, context) {
+          return GetMaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: MyTheme.mainTheme,
+            home: const SplashView(),
+          );
+        });
   }
 }
