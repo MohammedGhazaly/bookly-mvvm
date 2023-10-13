25bee460bedb7a0db58d3dac543419a2f7a0236e
MohammedGhazaly <warendragon67@gmail.com>
2023-10-13 20:17:53 +0200

    Added font family

--- lib/core/utils/my_theme.dart
+++ lib/core/utils/my_theme.dart
@@ -1,10 +1,12 @@
 import 'package:bookly_mvvm/core/utils/my_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MyTheme {
   static ThemeData mainTheme = ThemeData(
     scaffoldBackgroundColor: MyColors.kPrimaryColor,
     useMaterial3: true,
     brightness: Brightness.dark,
+    textTheme: GoogleFonts.poppinsTextTheme(ThemeData.dark().textTheme),
   );
 }
