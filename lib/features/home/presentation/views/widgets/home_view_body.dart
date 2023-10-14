39c04bf2de16b8eaf166926ee5a7b0db938a70f6
MohammedGhazaly <warendragon67@gmail.com>
2023-10-14 16:18:30 +0200

    Created book details view

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,11 +1,9 @@
 import 'package:bookly_mvvm/core/utils/my_styles.dart';
-import 'package:bookly_mvvm/features/home/presentation/views/widgets/best_seller_item.dart';
 import 'package:bookly_mvvm/features/home/presentation/views/widgets/best_seller_list_view.dart';
 import 'package:bookly_mvvm/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_mvvm/features/home/presentation/views/widgets/featured_books_list_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -17,7 +15,7 @@ class HomeViewBody extends StatelessWidget {
         SliverToBoxAdapter(
           child: SafeArea(
             child: Padding(
-              padding: EdgeInsets.symmetric(vertical: 20.h),
+              padding: EdgeInsets.only(top: 20.h),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
