5fb717cf21a84512ec31b49a9d3d338b715c038a
MohammedGhazaly <warendragon67@gmail.com>
2023-10-16 20:55:21 +0200

    Added custom scroll view to book details body

--- lib/features/home/presentation/views/widgets/book_details_view_widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_widgets/book_details_view_body.dart
@@ -2,6 +2,7 @@ import 'package:bookly_mvvm/core/utils/my_styles.dart';
 import 'package:bookly_mvvm/features/home/presentation/views/widgets/book_details_view_widgets/book_details_text.dart';
 import 'package:bookly_mvvm/features/home/presentation/views/widgets/book_details_view_widgets/books_action.dart';
 import 'package:bookly_mvvm/features/home/presentation/views/widgets/book_details_view_widgets/custom_book_details_app_bar.dart';
+import 'package:bookly_mvvm/features/home/presentation/views/widgets/book_details_view_widgets/similar_books_list_view.dart';
 import 'package:bookly_mvvm/features/home/presentation/views/widgets/home_view_widgets.dart/custom_book_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -11,81 +12,126 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Padding(
-        padding: EdgeInsets.only(top: 20.h),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 30.h),
-                child: CustomBookDetailsAppBar(),
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(
-                    horizontal: MediaQuery.of(context).size.width * 0.25),
-                child: CustomBookImage(),
-              ),
-              SizedBox(
-                height: 30.h,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 25.w),
-                child: BookDetailsText(),
-              ),
-              SizedBox(
-                height: 40.h,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20.w),
-                child: BooksAction(),
-              ),
-              const SizedBox(
-                height: 32,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 24.w),
-                child: Text(
-                  "You can also like",
-                  style: MyStyles.textStyle16
-                      .copyWith(fontWeight: FontWeight.w600),
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+            hasScrollBody: false,
+            child: SafeArea(
+              child: Padding(
+                padding: EdgeInsets.only(top: 20.h),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 30.h),
+                      child: CustomBookDetailsAppBar(),
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: MediaQuery.of(context).size.width * 0.25),
+                      child: CustomBookImage(),
+                    ),
+                    SizedBox(
+                      height: 30.h,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 25.w),
+                      child: BookDetailsText(),
+                    ),
+                    Expanded(
+                      child: SizedBox(
+                        height: 40.h,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20.w),
+                      child: BooksAction(),
+                    ),
+                    const SizedBox(
+                      height: 32,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 24.w),
+                      child: Text(
+                        "You can also like",
+                        style: MyStyles.textStyle16
+                            .copyWith(fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    SimilarBooksListView(),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                  ],
                 ),
               ),
-              const SizedBox(
-                height: 16,
-              ),
-              SimilarBooksListView(),
-              const SizedBox(
-                height: 40,
-              ),
-            ],
-          ),
-        ),
-      ),
+            ))
+      ],
     );
-  }
-}
 
-class SimilarBooksListView extends StatelessWidget {
-  const SimilarBooksListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.15,
-      child: ListView.builder(
-          itemCount: 10,
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            return const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 10),
-              child: CustomBookImage(),
-            );
-          }),
-    );
+    // return SafeArea(
+    //   child: Padding(
+    //     padding: EdgeInsets.only(top: 20.h),
+    //     child: SingleChildScrollView(
+    //       child: Column(
+    //         crossAxisAlignment: CrossAxisAlignment.start,
+    //         children: [
+    //           Padding(
+    //             padding: EdgeInsets.symmetric(horizontal: 30.h),
+    //             child: CustomBookDetailsAppBar(),
+    //           ),
+    //           const SizedBox(
+    //             height: 12,
+    //           ),
+    //           Padding(
+    //             padding: EdgeInsets.symmetric(
+    //                 horizontal: MediaQuery.of(context).size.width * 0.25),
+    //             child: CustomBookImage(),
+    //           ),
+    //           SizedBox(
+    //             height: 30.h,
+    //           ),
+    //           Padding(
+    //             padding: EdgeInsets.symmetric(horizontal: 25.w),
+    //             child: BookDetailsText(),
+    //           ),
+    //           Expanded(
+    //             child: SizedBox(
+    //               height: 40.h,
+    //             ),
+    //           ),
+    //           Padding(
+    //             padding: EdgeInsets.symmetric(horizontal: 20.w),
+    //             child: BooksAction(),
+    //           ),
+    //           const SizedBox(
+    //             height: 32,
+    //           ),
+    //           Padding(
+    //             padding: EdgeInsets.symmetric(horizontal: 24.w),
+    //             child: Text(
+    //               "You can also like",
+    //               style: MyStyles.textStyle16
+    //                   .copyWith(fontWeight: FontWeight.w600),
+    //             ),
+    //           ),
+    //           const SizedBox(
+    //             height: 16,
+    //           ),
+    //           SimilarBooksListView(),
+    //           const SizedBox(
+    //             height: 40,
+    //           ),
+    //         ],
+    //       ),
+    //     ),
+    //   ),
+    // );
   }
 }
