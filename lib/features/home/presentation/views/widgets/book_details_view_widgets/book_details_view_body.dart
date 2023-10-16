fb52661610fac715497b26548129bf3899fc11c4
MohammedGhazaly <warendragon67@gmail.com>
2023-10-16 20:39:24 +0200

    Added similar books listview

--- lib/features/home/presentation/views/widgets/book_details_view_widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_widgets/book_details_view_body.dart
@@ -1,4 +1,4 @@
-import 'package:bookly_mvvm/core/shared_widgets/custom_button.dart';
+import 'package:bookly_mvvm/core/utils/my_styles.dart';
 import 'package:bookly_mvvm/features/home/presentation/views/widgets/book_details_view_widgets/book_details_text.dart';
 import 'package:bookly_mvvm/features/home/presentation/views/widgets/book_details_view_widgets/books_action.dart';
 import 'package:bookly_mvvm/features/home/presentation/views/widgets/book_details_view_widgets/custom_book_details_app_bar.dart';
@@ -14,34 +14,78 @@ class BookDetailsViewBody extends StatelessWidget {
     return SafeArea(
       child: Padding(
         padding: EdgeInsets.only(top: 20.h),
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 30.h),
-              child: CustomBookDetailsAppBar(),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(
-                  horizontal: MediaQuery.of(context).size.width * 0.25),
-              child: CustomBookImage(),
-            ),
-            SizedBox(
-              height: 30.h,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 25.w),
-              child: BookDetailsText(),
-            ),
-            SizedBox(
-              height: 40.h,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20.w),
-              child: BooksAction(),
-            )
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30.h),
+                child: CustomBookDetailsAppBar(),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: MediaQuery.of(context).size.width * 0.25),
+                child: CustomBookImage(),
+              ),
+              SizedBox(
+                height: 30.h,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25.w),
+                child: BookDetailsText(),
+              ),
+              SizedBox(
+                height: 40.h,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.w),
+                child: BooksAction(),
+              ),
+              const SizedBox(
+                height: 32,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24.w),
+                child: Text(
+                  "You can also like",
+                  style: MyStyles.textStyle16
+                      .copyWith(fontWeight: FontWeight.w600),
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              SimilarBooksListView(),
+              const SizedBox(
+                height: 40,
+              ),
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class SimilarBooksListView extends StatelessWidget {
+  const SimilarBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.15,
+      child: ListView.builder(
+          itemCount: 10,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: CustomBookImage(),
+            );
+          }),
     );
   }
 }
--- lib/features/home/presentation/views/widgets/book_details_view_widgets/similar_books_list_view.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_widgets/similar_books_list_view.dart
@@ -1,13 +1,13 @@
 import 'package:bookly_mvvm/features/home/presentation/views/widgets/home_view_widgets.dart/custom_book_image.dart';
 import 'package:flutter/material.dart';
 
-class FeaturedBooksListView extends StatelessWidget {
-  const FeaturedBooksListView({super.key});
+class SimilarBooksListView extends StatelessWidget {
+  const SimilarBooksListView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.3,
+      height: MediaQuery.of(context).size.height * 0.15,
       child: ListView.builder(
           itemCount: 10,
           scrollDirection: Axis.horizontal,
