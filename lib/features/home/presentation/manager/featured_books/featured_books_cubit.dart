fcca5dcc52889608189609fd51285aa697661586
MohammedGhazaly <warendragon67@gmail.com>
2023-10-20 15:49:59 +0200

    Created featured books method

--- lib/features/home/presentation/manager/featured_books/featured_books_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-
-part 'featured_books_state.dart';
-
-class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
-}
