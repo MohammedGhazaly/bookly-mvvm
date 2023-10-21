208d9a40b2f4ec1306f21ed5b817a6a2f9ef868d
MohammedGhazaly <warendragon67@gmail.com>
2023-10-21 19:12:50 +0200

    Added cached network image for featured books

--- lib/features/home/presentation/views/widgets/home_view_widgets.dart/custom_book_image.dart
+++ lib/features/home/presentation/views/widgets/home_view_widgets.dart/custom_book_image.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_mvvm/core/utils/my_assets.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class CustomBookImage extends StatelessWidget {
@@ -11,15 +12,14 @@ class CustomBookImage extends StatelessWidget {
       // height: MediaQuery.of(context).size.height * 0.25,
       child: AspectRatio(
         aspectRatio: 2.6 / 4,
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            image: DecorationImage(
-              fit: BoxFit.fill,
-              image: NetworkImage(
-                bookImage,
-              ),
-            ),
+        child: ClipRRect(
+          borderRadius: const BorderRadius.all(Radius.circular(10)),
+          child: CachedNetworkImage(
+            imageUrl: bookImage,
+            fit: BoxFit.cover,
+            errorWidget: (context, url, error) {
+              return const Icon(Icons.error);
+            },
           ),
         ),
       ),
